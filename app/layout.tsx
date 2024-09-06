acaea9f45aa64b806d38232fd2f99157132277a0
gawandeabhishek <abhishekgawande1667@gmail.com>
2024-09-07 00:25:28 +0530

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative px-2 min-h-[98vh]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative px-2 min-h-[98dvh]`}
       >
         <Navbar />
         {children}
